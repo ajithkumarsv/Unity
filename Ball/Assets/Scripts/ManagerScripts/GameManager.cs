0509bc4103913bc57315a13be3cc86e2618d4680
ajithkumarsv <ajithkumarsv66@gmail.com>
2021-12-03 21:49:30 +0530

    Ball updated

--- Ball/Assets/Scripts/ManagerScripts/GameManager.cs
+++ Ball/Assets/Scripts/ManagerScripts/GameManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private static GameManager _instance;
+
+    public static GameManager Instance{
+        get{
+            if(!_instance) _instance=FindObjectOfType<GameManager>();
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Ball/Assets/Scripts/ManagerScripts/UIManager.cs
+++ Ball/Assets/Scripts/ManagerScripts/UIManager.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    private static UIManager _instance;
+
+    public static UIManager Instance{
+        get{
+            if(!_instance) _instance=FindObjectOfType<UIManager>();
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject MenuScreen;
+    public GameObject PreloaderScreen;
+    public GameObject PauseScreen;
+    public GameObject LoadingScreen;
+    public GameObject GameplayScreen;
+
+    public void Awake() {
+        _instance=this;
+    }
+    private void Start() {
+        Load();
+    }
+
+    private void Load()
     {
-        
+        PreloaderScreen.SetActive(true);
+        PauseScreen.SetActive(false);
+        LoadingScreen.SetActive(false);
+        GameplayScreen.SetActive(false);
+        MenuScreen.SetActive(false);
+        OnLoadCompleted();
+    }
+
+    private void OnLoadCompleted()
+    {
+        PreloaderScreen.SetActive(false);
+        MenuScreen.SetActive(true);
     }
 }
